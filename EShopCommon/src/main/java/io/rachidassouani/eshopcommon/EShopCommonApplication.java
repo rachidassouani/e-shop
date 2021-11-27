35330edc07dcc0df264fd1b649b0e73b76255f67
rachidassouani <rachidassouani@gmail.com>
2021-11-27 16:32:07 +0100

    first commit

---
+++ EShopCommon/src/main/java/io/rachidassouani/eshopcommon/EShopCommonApplication.java
@@ -0,0 +1,13 @@
+package io.rachidassouani.eshopcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShopCommonApplication.class, args);
+	}
+
+}
---
+++ EShopWebParent/EShopFrontEnd/src/main/java/io/rachidassouani/eshopfrontend/EShopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package io.rachidassouani.eshopfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShopFrontEndApplication.class, args);
+	}
+
+}
