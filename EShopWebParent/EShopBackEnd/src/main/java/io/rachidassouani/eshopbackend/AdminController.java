c93fa23505a7708608502b07b58762004379f753
rachidassouani <rachidassouani@gmail.com>
2021-11-27 20:08:47 +0100

    added the navigation bar and the necessary navigation links for the administration portal

---
+++ EShopWebParent/EShopBackEnd/src/main/java/io/rachidassouani/eshopbackend/AdminController.java
@@ -0,0 +1,13 @@
+package io.rachidassouani.eshopbackend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+	@GetMapping("")
+	public String index() {
+		return "index";
+	}
+}
