bdc7cfdff21054a82c28b7920d40cbbc063a1c3c
rachidassouani <rachidassouani@gmail.com>
2022-09-28 23:16:59 +0100

    Added a login page and an authentication required for all requests.

--- EShopWebParent/EShopBackEnd/src/main/java/io/rachidassouani/eshopbackend/AdminController.java
+++ EShopWebParent/EShopBackEnd/src/main/java/io/rachidassouani/eshopbackend/AdminController.java
@@ -10,4 +10,9 @@ public class AdminController {
 	public String index() {
 		return "index";
 	}
+	
+	@GetMapping("login")
+	public String login() {
+		return "login";
+	}
 }
--- EShopWebParent/EShopBackEnd/src/main/java/io/rachidassouani/eshopbackend/security/SecurityConfig.java
+++ EShopWebParent/EShopBackEnd/src/main/java/io/rachidassouani/eshopbackend/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.rachidassouani.eshopbackend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +20,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().anyRequest().authenticated()
+		.and()
+		.formLogin()
+			.loginPage("/login")
+			.permitAll();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
 	}
 }
