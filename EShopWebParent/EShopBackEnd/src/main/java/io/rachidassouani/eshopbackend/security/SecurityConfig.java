899ea38c03f345c6ca9d0c2ba4d75367ca66baa8
rachidassouani <rachidassouani@gmail.com>
2023-04-14 16:17:49 +0000

    Authorized these roles Admin, Editor, SalesPerson and Shipper to consult the list of products

--- EShopWebParent/EShopBackEnd/src/main/java/io/rachidassouani/eshopbackend/security/SecurityConfig.java
+++ EShopWebParent/EShopBackEnd/src/main/java/io/rachidassouani/eshopbackend/security/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 			.antMatchers("/users/**").hasAuthority("Admin")
 			.antMatchers("/categories/**", "/brands/**").hasAnyAuthority("Admin", "Editor")
+			.antMatchers("/products/**").hasAnyAuthority("Admin", "Editor", "SalesPerson", "Shipper")
 			.anyRequest().authenticated()
 		.and()
 			.formLogin().loginPage("/login")
